80213ecba26e1409ca7bf6adf773bd65d5e6d591
Anirban Ray <aniroycr7@gmail.com>
2023-02-27 20:09:41 +0530

    almost

--- weather/src/App.js
+++ weather/src/App.js
@@ -5,68 +5,64 @@ import { useEffect } from 'react'
 import Weathercom from './Weathercom'
 
 const App = () => {
-   const [city,setCity]=useState("")
-   
-   const [data,setData]=useState([])
-   const [weather,setWeather]=useState(0)
-      const api ="1d4097e9f7d10574cecced005e84c574"
+  const [city, setCity] = useState("");
+  const [data, setData] = useState([]);
+  const [weather, setWeather] = useState(0);
+  const [searchHistory, setSearchHistory] = useState([]);
 
-   const getData=(cityname)=>{
-        if(!cityname)
-          return 
-          const url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityname +"&appid="+api;
-          axios.get(url,setWeather(weather+20))
-          .then((res)=>{
-            setData(res.data)
-          }).catch((err)=>{
-               console.log("err",err)
-          })
-          setWeather(weather+70)
-        
-   }
+  const api = "1d4097e9f7d10574cecced005e84c574";
 
-   const handleChange=(e)=>{
-       setCity(e.target.value)
-   }
-   const handleSearch=()=>{
-      getData(city)
-      setCity("")
-   }
+  const getData = (cityname) => {
+    if (!cityname) return;
 
-   useEffect(()=>{
-    getData("Kolkata")
-   },[data])
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityname + "&appid=" + api;
+    axios.get(url)
+      .then((res) => {
+        setData(res.data);
+        setWeather(weather + 20);
+        setSearchHistory([...searchHistory.slice(-2), res.data.name]);
+      })
+      .catch((err) => {
+        if (err.response && err.response.status === 404) {
+          alert("Invalid city");
+        } else {
+          console.log("err", err);
+        }
+      });
+  }
+
+  const handleChange = (e) => {
+    setCity(e.target.value);
+  }
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    getData(city);
+    setCity("");
+  }
+
+  useEffect(() => {
+    getData("Kolkata");
+  }, []);
 
   return (
     <div>
       <div>
         <h1>Weather App</h1>
         <form onSubmit={handleSearch}>
-        <input type="text" onChange={handleChange} value={city} name="Search"  ></input>
-        <button type='submit'>click</button>
+          <input type="text" onChange={handleChange} value={city} name="Search" placeholder="Enter city name" />
+          <button type='submit'>Search</button>
         </form>
-        {/* <input type="text" onChange={handleChange} value={city} name="Search" ></input> */}
-        {/* <button onClick={handleSearch}>click</button> */}
-       
-           <p>Wheather Details of {data?.name}</p>
-           <p>Current temparature {(data?.main?.temp - 273.15).toFixed(1)}°C</p> 
-           <p> Temparature range {(data?.main?.temp_max - 273.15).toFixed(1)}°C to  {(data?.main?.temp_min - 273.15).toFixed(1)}°C </p> 
-          <p>Humidity:{data?.main?.humidity}</p> 
-          <p>Sea level: {((data?.main?.temp - 273.15).toFixed(1))*40}</p>
-          <p>Ground level: {((data?.main?.temp - 273.15).toFixed(1))*36}</p>
-
-          <p></p>
-
-
-
-
-
-           
+        <p>Previous 3 searched cities: {searchHistory.map((city) => city + " ")}</p>
+        <p>Weather Details of {data?.name}</p>
+        <p>Current temperature {(data?.main?.temp - 273.15).toFixed(1)}°C</p>
+        <p>Temperature range {(data?.main?.temp_max - 273.15).toFixed(1)}°C to {(data?.main?.temp_min - 273.15).toFixed(1)}°C</p>
+        <p>Humidity: {data?.main?.humidity}</p>
+        <p>Sea level: {((data?.main?.temp - 273.15).toFixed(1)) * 40}</p>
+        <p>Ground level: {((data?.main?.temp - 273.15).toFixed(1)) * 36}</p>
       </div>
-
-      
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
